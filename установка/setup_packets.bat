391e4f6e3f4548cb362180f2b88e3520a3b620fb
Pasha-blinDeveloper <gprod121314@gmail.com>
2020-02-28 15:28:25 +0300

    Добавил установщик всех пакетов и инструкцию
    Добавил установщик в разрешении bat устанавливает пакеты необходимые для функциональностьи афины и описание процесса установки

---
+++ установка/setup_packets.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Loading packages
+pip install colorama
+pip install pyowm 
+pip install pyttsx3
+pip install SpeechRecognition 
+pip install PyAudio-0.2.11-cp37-cp37m-win32.whl
+pip install PyAudio-0.2.11-cp37-cp37m-win_amd64.whl
+pip install pypiwin32
